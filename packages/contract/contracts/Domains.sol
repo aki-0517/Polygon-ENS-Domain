5f225c32092c29db8612236b7b3b007bb733ed99
aki-0517 <akki.pp.777@gmail.com>
2023-12-22 21:07:37 +0900

    edit 13

--- packages/contract/contracts/Domains.sol
+++ packages/contract/contracts/Domains.sol
@@ -4,7 +4,32 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract Domains {
-  constructor() {
-    console.log("THIS IS MY DOMAINS CONTRACT. NICE.");
-  }
+    mapping(string => address) public domains;
+
+    mapping(string => string) public records;
+
+    constructor() {
+        console.log("Yo yo, I am a contract and I am smart");
+    }
+
+    function register(string calldata name) public {
+        require(domains[name] == address(0));
+        domains[name] = msg.sender;
+        console.log("%s has registered a domain!", msg.sender);
+    }
+
+    function getAddress(string calldata name) public view returns (address) {
+        return domains[name];
+    }
+
+    function setRecord(string calldata name, string calldata record) public {
+        require(domains[name] == msg.sender);
+        records[name] = record;
+    }
+
+    function getRecord(
+        string calldata name
+    ) public view returns (string memory) {
+        return records[name];
+    }
 }
